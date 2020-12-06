3a5c84a08a49d3243df3f654a6b8ce0b760fd41a
soloplayers <937059391@qq.com>
2020-12-06 19:34:40 +0800

    deploy

---
+++ precache-manifest.a5bb8eaaa3b64ebb3a1d43f91bd9342f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e29462cd9cd14290885",
+    "url": "/keep-account-website2/css/app.32e90dc2.css"
+  },
+  {
+    "revision": "adad8b6464b71988c9c9ca47f0c60ac2",
+    "url": "/keep-account-website2/index.html"
+  },
+  {
+    "revision": "4e29462cd9cd14290885",
+    "url": "/keep-account-website2/js/app.9242e686.js"
+  },
+  {
+    "revision": "c5a42e0e4e359bb5c1c2",
+    "url": "/keep-account-website2/js/chunk-vendors.62eaa98d.js"
+  },
+  {
+    "revision": "d69b110ea9e408c9366c9265e8677551",
+    "url": "/keep-account-website2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-account-website2/robots.txt"
+  }
+]);
